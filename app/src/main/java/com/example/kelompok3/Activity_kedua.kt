45e3bc8f1ec6e692fe86c8482b5fc7eb3c08ca2b
herry88 <herry081288@gmail.com>
2025-09-09 16:27:45 +0700

    done

--- app/src/main/java/com/example/kelompok3/Activity_kedua.kt
+++ app/src/main/java/com/example/kelompok3/Activity_kedua.kt
@@ -1,16 +1,22 @@
 package com.example.kelompok3
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class Activity_kedua : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_kedua)
-        
+
+        val btnklik = findViewById<Button>(R.id.button)
+
+        btnklik.setOnClickListener {
+            val intent = Intent(this, ActivityKetiga::class.java)
+            startActivity(intent)
+        }
     }
 }
